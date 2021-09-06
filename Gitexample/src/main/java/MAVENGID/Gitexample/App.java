3c2d91a4eba3a3d6fa0f9148aedc5d563a73ab48
sowmmya <sowmmya@localhost>
2021-09-06 17:41:30 +0530

    Testing GIT

---
+++ Gitexample/src/main/java/MAVENGID/Gitexample/App.java
@@ -0,0 +1,13 @@
+package MAVENGID.Gitexample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

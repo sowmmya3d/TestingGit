fe8614dc38f37d2783f9bd84e2c75d931d83d046
sowmmya <sowmmya@localhost>
2021-09-10 09:20:48 +0530

    Added test cases

--- Gitexample/src/test/java/MAVENGID/Gitexample/GitTesting.java
+++ Gitexample/src/test/java/MAVENGID/Gitexample/GitTesting.java
@@ -1,10 +1,19 @@
 package MAVENGID.Gitexample;
 
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
 public class GitTesting {
+	@Test
+	public void sample()
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	{
+		System.out.println("Sample");
+	}
+	
+	public void testAdd() {
+		assertEquals(1 + 1, 2);
 	}
 
 }
